d58dcfd41fc2b6212514ce6e1f6f98d008a08350
Predator <evanslance100@gmail.com>
2024-12-20 14:59:49 +0300

    configure header, feature and info compnents

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,32 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 import './navbar.css';
-import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
-const Navbar = () => {
-  const {toggleMenu, setToggleMenu} = useState(false);
+
+const Menu = () => {
   return (
-    /*BEM -> Block element modifier class naming convention used*/
+    <>
+      <p><a href="#">Home</a></p>
+      <p><a href="#wgpt3">What is GPT?</a></p>
+      <p><a href="#possibility">Open AI</a></p>
+      <p><a href="#features">Case Studies</a></p>
+      <p><a href="#blog">Library</a></p>
+    </>
+  );
+};
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    /* BEM -> Block element modifier class naming convention used */
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'>
-          <img src={logo} alt='logo'/>
+          <img src={logo} alt='logo' />
         </div>
         <div className='gpt3__navbar-links_container'>
-          <p><a href="#">Home</a></p>
-          <p><a href="#wgpt3">What is GPT?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+          <Menu />
         </div>
       </div>
       <div className='gpt3__navbar-sign'>
@@ -24,24 +34,25 @@ const Navbar = () => {
         <button type='button'>Sign up</button>
       </div>
       <div className='gpt3__navbar-menu'>
-        {toggleMenu
-        ? <RiCloseLine color="#ffff" size={27} onclick={()=> setToggleMenu(false)}/>
-        : <RiMenu3Line color="#ffff" size={27} onclick={()=> setToggleMenu(true)}/>
-      }
-      {toggleMenu && (
-        <div className='gpt3__navbar-menu_container scale-up-center'>
-          <div className='gpt3__navbar-menu_container-links'>
-          <p><a href="#">Home</a></p>
-          <p><a href="#wgpt3">What is GPT?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+        {toggleMenu ? (
+          <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+        ) : (
+          <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        )}
+        {toggleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'>
+              <Menu />
+              <div className='gpt3__navbar-menu_container-links-sign'>
+                <p>Sign in</p>
+                <button type='button'>Sign up</button>
+              </div>
+            </div>
           </div>
-        </div>
-      )}
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className='gpt3__header-content'>
+        <h1 className='gradient__text'>Let's build Something Amazing with GPT-3 OpenAI</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae libero labore commodi voluptas. Asperiores fugiat architecto hic ullam tempora mollitia ipsa ab? Velit eaque modi quia repellat .</p>
+        <div className='gpt3__header-content__input'>
+          <input type="email" placeholder='Your Email Address' />
+          <button type='button'>Get Started</button>
+        </div>
+        <div className='gpt3__header-content__people'>
+          <img src={people} alt="peole" />
+          <p>1,600 people requested access in the last 24 hours</p>
+        </div>
+      </div>  
+      <div className='gpt3__header-image'>
+        <img src={ai} alt="ai" />
+      </div>    
+    </div>
   )
 }
 
